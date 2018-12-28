e50f5824cf8dde2e2832b4f711e40bd7f46fa563
BPDesignsPNB <www.pablobeccalli@gmail.com>
2018-12-28 09:43:58 -0300

    Updates

---
+++ precache-manifest.9332fedd47fd6f952285a3c01216cf50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ad2752b4eb47f9553ef71f29230b1c2",
+    "url": "/React-Eventos/static/media/pattern.4ad2752b.png"
+  },
+  {
+    "revision": "568d5628357905d87447",
+    "url": "/React-Eventos/static/js/runtime~main.568d5628.js"
+  },
+  {
+    "revision": "eaaa97e32367e938ae0d",
+    "url": "/React-Eventos/static/js/main.eaaa97e3.chunk.js"
+  },
+  {
+    "revision": "97ca4db8f2be9dd7f8d1",
+    "url": "/React-Eventos/static/js/1.97ca4db8.chunk.js"
+  },
+  {
+    "revision": "eaaa97e32367e938ae0d",
+    "url": "/React-Eventos/static/css/main.71c71344.chunk.css"
+  },
+  {
+    "revision": "513c84400b909f2e4641d0971b91c589",
+    "url": "/React-Eventos/index.html"
+  }
+];
